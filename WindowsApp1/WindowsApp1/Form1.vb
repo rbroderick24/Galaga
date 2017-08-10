fc1b181ab87310d962be72a768b9b811a14f5f7f
Sheldor1 <diamborsio124>
2017-08-10 11:45:19 -0400

    commit

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -3,6 +3,7 @@
 
         Dim bullet As New Pen(Color.Red, 3)
 
+
         Dim P1Left As Boolean = False
         Dim P1Right As Boolean = False
         Dim P1Up As Boolean = False
@@ -13,5 +14,11 @@
         Dim P2Down As Boolean = False
         Dim P1Position As New Point(390, 800)
 
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.keyChar = "0" Then
+
+        End If
     End Sub
 End Class
