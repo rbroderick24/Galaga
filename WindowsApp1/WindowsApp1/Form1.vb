91c10f3d55fd992ef8fc6bd3259b5920f77db5ba
Sheldor1 <diamborsio124>
2017-08-10 11:32:02 -0400

    gggg
    gggg

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,3 +1,7 @@
 ﻿Public Class Form1
+    Dim shooting As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim bullet As New Pen
 
+    End Sub
 End Class
