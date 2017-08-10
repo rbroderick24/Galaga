671abee930b2442ddee3d0916083d13572b95d34
Sheldor1 <diamborsio124>
2017-08-10 11:41:15 -0400

    Commit

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Form1
     Dim shooting As Boolean = False
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim bullet As New Pen
 
     End Sub
 End Class
