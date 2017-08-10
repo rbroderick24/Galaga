40f99f2bf76f02571ce21ab9c08409f224df9c71
Prabhalaa2 <30726851+Prabhalaa2@users.noreply.github.com>
2017-08-10 11:45:10 -0400

    Galaga

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,17 +1,25 @@
 ﻿Public Class Form1
+    Dim P1Left As Boolean = False
+    Dim P1Right As Boolean = False
+    Dim P1Up As Boolean = False
+    Dim P1Down As Boolean = False
+    Dim P2Left As Boolean = False
+    Dim P2Right As Boolean = False
+    Dim P2Up As Boolean = False
+    Dim P2Down As Boolean = False
+    Dim P1Position As New Point(390, 800)
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         Dim bullet As New Pen(Color.Red, 3)
+    End Sub
 
-        Dim P1Left As Boolean = False
-        Dim P1Right As Boolean = False
-        Dim P1Up As Boolean = False
-        Dim P1Down As Boolean = False
-        Dim P2Left As Boolean = False
-        Dim P2Right As Boolean = False
-        Dim P2Up As Boolean = False
-        Dim P2Down As Boolean = False
-        Dim P1Position As New Point(390, 800)
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = "A" Then
+            P1Left = True
+        End If
+    End Sub
+
+    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
 
     End Sub
 End Class
