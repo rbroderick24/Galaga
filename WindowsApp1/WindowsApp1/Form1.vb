734f045b46e4762bd9a2325514782c8341afd0f0
Sheldor1 <diamborsio124>
2017-08-10 11:49:25 -0400

    commit

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,4 +1,6 @@
 ﻿Public Class Form1
+    Dim shooting As Boolean = False
+    Dim bullet As New Pen(Color.Red, 3)
     Dim P1Left As Boolean = False
     Dim P1Right As Boolean = False
     Dim P1Up As Boolean = False
@@ -10,35 +12,36 @@
     Dim P1Position As New Point(390, 800)
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        Dim bullet As New Pen(Color.Red, 3)
+
     End Sub
 
     Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
         If e.KeyChar = "A" Then
             P1Left = True
         End If
-    End Sub
+        If e.KeyChar = "0" Then
+            shooting = True
+            If shooting = True Then
 
-<<<<<<< HEAD
+            End If
 
-        Dim P1Left As Boolean = False
-        Dim P1Right As Boolean = False
-        Dim P1Up As Boolean = False
-        Dim P1Down As Boolean = False
-        Dim P2Left As Boolean = False
-        Dim P2Right As Boolean = False
-        Dim P2Up As Boolean = False
-        Dim P2Down As Boolean = False
-        Dim P1Position As New Point(390, 800)
-=======
-    Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
->>>>>>> 40f99f2bf76f02571ce21ab9c08409f224df9c71
-
-    End Sub
-
-    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
-        If e.keyChar = "0" Then
 
         End If
+    End Sub
+
+
+
+    Dim P1Left As Boolean = False
+    Dim P1Right As Boolean = False
+    Dim P1Up As Boolean = False
+    Dim P1Down As Boolean = False
+    Dim P2Left As Boolean = False
+    Dim P2Right As Boolean = False
+    Dim P2Up As Boolean = False
+    Dim P2Down As Boolean = False
+    Dim P1Position As New Point(390, 800)
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+
     End Sub
 End Class
