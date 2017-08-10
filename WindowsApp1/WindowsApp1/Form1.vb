46e84a45cb740caf5d4ada1020bffe3ac6f89bd2
Sheldor1 <diamborsio124>
2017-08-10 11:41:26 -0400

    commit

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -1,6 +1,7 @@
 ﻿Public Class Form1
     Dim shooting As Boolean = False
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Dim bullet As New Pen(Color.Red, 3)
+        If DivideByZeroException.KeyChar Then
     End Sub
 End Class
