5f67f9a4db44e7ad2e227cf8afe130ee4bcd2dc0
1030shruti <shruti.d.mandrekar@gmail.com>
2017-08-11 11:11:17 -0400

    Movements

--- WindowsApp1/WindowsApp1/Form1.Designer.vb
+++ WindowsApp1/WindowsApp1/Form1.Designer.vb
@@ -23,46 +23,24 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(12, 371)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Label1"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(57, 371)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(39, 13)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Label2"
-        '
         'Timer1
         '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(1159, 451)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(764, 811)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Label1 As Label
-    Friend WithEvents Label2 As Label
     Friend WithEvents Timer1 As Timer
 End Class
--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -30,16 +30,16 @@
         If e.KeyCode = 83 Then
             P1Down = True
         End If
-        If e.KeyCode = 49 Then
+        If e.KeyCode = 37 Then
             P2Left = True
         End If
-        If e.KeyCode = 51 Then
+        If e.KeyCode = 39 Then
             P2Right = True
         End If
-        If e.KeyCode = 53 Then
+        If e.KeyCode = 38 Then
             P2Up = True
         End If
-        If e.KeyCode = 50 Then
+        If e.KeyCode = 40 Then
             P2Down = True
         End If
 
@@ -77,30 +77,50 @@
         If e.KeyCode = 83 Then
             P1Down = False
         End If
-        If e.KeyCode = 49 Then
+        If e.KeyCode = 37 Then
             P2Left = False
         End If
-        If e.KeyCode = 51 Then
+        If e.KeyCode = 39 Then
             P2Right = False
         End If
-        If e.KeyCode = 53 Then
+        If e.KeyCode = 38 Then
             P2Up = False
         End If
-        If e.KeyCode = 50 Then
+        If e.KeyCode = 40 Then
             P2Down = False
         End If
     End Sub
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs)
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim point1 As New Point(P1Position.X - 50, 800)
         Dim point2 As New Point(P1Position.X + 50, 800)
         Dim point3 As New Point(P1Position.X, P1Position.Y - 25)
+        Dim point4 As New Point(P2Position.X - 50, 50)
+        Dim point5 As New Point(P2Position.X + 50, 50)
+        Dim point6 As New Point(P2Position.X, P2Position.Y + 25)
+
         Dim draw As System.Drawing.Graphics = Me.CreateGraphics
         Dim allP As Point() = {point1, point2, point3}
+        Dim allP2 As Point() = {point4, point5, point6}
         Dim redBrush As New SolidBrush(Color.Red)
-        draw.FillPolygon(redBrush, allP)
+        Dim blueBrush As New SolidBrush(Color.Blue)
 
-        Label1.Text = MousePosition.X
-        Label2.Text = MousePosition.Y
+        draw.FillRectangle(Brushes.Black, 0, 0, 780, 850)
+        draw.FillPolygon(redBrush, allP)
+        draw.FillPolygon(blueBrush, allP2)
+
+        If P1Left Then
+            P1Position.X -= 10
+        End If
+        If P1Right Then
+            P1Position.X += 10
+        End If
+
+        If P2Left Then
+            P2Position.X -= 10
+        End If
+        If P2Right Then
+            P2Position.X += 10
+        End If
     End Sub
 End Class
