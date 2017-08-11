9714b3712a2ad20076a51851c127478fb90a7909
1030shruti <shruti.d.mandrekar@gmail.com>
2017-08-11 10:18:09 -0400

    Fixed errors

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -15,7 +15,6 @@
     Dim P2Position As New Point(390, 50)
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-
     End Sub
     Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
         Dim draw As System.Drawing.Graphics = Me.CreateGraphics
@@ -48,32 +47,12 @@
             shooting = True
             If shooting = True Then
 
-
                 draw.DrawLine(bullet, 300, 100, 50, 50)
-
-
-
 
                 draw.DrawLine(bullet, 50, 100, 50, 50)
 
                 draw.DrawLine(bullet, 20000, 20000, 100, 100)
-<<<<<<< HEAD
 
-
-=======
-
-<<<<<<< HEAD
-=======
-
-
-
-
-
-
-
->>>>>>> bbfdb51e05316c0ad89c2f6be807303556ccca79
-
->>>>>>> 21def598d18d957920d5ea2dda9ef0a1ee85b718
                 If e.KeyCode = 32 Then
                     shooting = True
                     If shooting = True Then
@@ -84,50 +63,6 @@
             End If
         End If
     End Sub
-
-
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        Label1.Text = MousePosition.X
-        Label2.Text = MousePosition.Y
-
-    End Sub
-
-
-
-    'Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
-    'If e.KeyChar = "A" Then
-    '    P1Left = False
-    'End If
-    'If e.KeyChar = "D" Then
-    '    P1Right = False
-    'End If
-    'If e.KeyChar = "W" Then
-    '    P1Up = False
-    'End If
-    'If e.KeyChar = "S" Then
-    '    P1Down = False
-    'End If
-    'If e.KeyChar = "1" Then
-    '    P2Left = False
-    'End If
-    'If e.KeyChar = "3" Then
-    '    P2Right = False
-    'End If
-    'If e.KeyChar = "5" Then
-    '    P2Up = False
-    'End If
-    'If e.KeyChar = "2" Then
-    '    P2Down = False
-    'End If
-
-    'End Sub
-
-<<<<<<< HEAD
-
-
-
-=======
->>>>>>> bbfdb51e05316c0ad89c2f6be807303556ccca79
 
     Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
         If e.KeyCode = 65 Then
@@ -155,7 +90,6 @@
             P2Down = False
         End If
     End Sub
-<<<<<<< HEAD
 
     Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
         Dim point1 As New Point(P1Position.X - 50, 800)
@@ -165,11 +99,8 @@
         Dim allP As Point() = {point1, point2, point3}
         Dim redBrush As New SolidBrush(Color.Red)
         draw.FillPolygon(redBrush, allP)
-    End Sub
-=======
-<<<<<<< HEAD
-=======
 
->>>>>>> bbfdb51e05316c0ad89c2f6be807303556ccca79
->>>>>>> 21def598d18d957920d5ea2dda9ef0a1ee85b718
+        Label1.Text = MousePosition.X
+        Label2.Text = MousePosition.Y
+    End Sub
 End Class
