f3b2d7d0f6531527456be321e7ffbb67d64e311b
rbroderick24 <30726596+rbroderick24@users.noreply.github.com>
2017-08-11 09:29:58 -0400

    commit

--- WindowsApp1/WindowsApp1/Form1.vb
+++ WindowsApp1/WindowsApp1/Form1.vb
@@ -14,7 +14,60 @@
     Dim P1Position As New Point(390, 800)
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+
     End Sub
+
+    Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
+        If e.KeyChar = "A" Then
+            P1Left = True
+        End If
+        If e.KeyChar = "D" Then
+            P1Right = True
+        End If
+        If e.KeyChar = "W" Then
+            P1Up = True
+        End If
+        If e.KeyChar = "S" Then
+            P1Down = True
+        End If
+        If e.KeyChar = "1" Then
+            P2Left = True
+        End If
+        If e.KeyChar = "3" Then
+            P2Right = True
+        End If
+        If e.KeyChar = "5" Then
+            P2Up = True
+        End If
+        If e.KeyChar = "2" Then
+            P2Down = True
+        End If
+
+        If e.KeyChar = "0" Then
+            shooting = True
+            If shooting = True Then
+                draw.DrawLine(bullet, 20000, 20000, 100, 100)
+
+
+
+
+
+
+
+
+                If e.KeyChar = "Spacebar" Then
+                    shooting = True
+                    If shooting = True Then
+                        draw.DrawLine(bullet2, 10, 10, 10, 10)
+
+
+                    End If
+
+                End If
+            End If
+        End If
+    End Sub
+
     Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
         If e.KeyCode = 65 Then
             P1Left = False
@@ -28,59 +81,17 @@
         If e.KeyCode = 83 Then
             P1Down = False
         End If
-        If e.KeyCode = 37 Then
+        If e.KeyCode = 49 Then
             P2Left = False
         End If
-        If e.KeyCode = 39 Then
+        If e.KeyCode = 51 Then
             P2Right = False
         End If
-        If e.KeyCode = 38 Then
+        If e.KeyCode = 53 Then
             P2Up = False
         End If
-        If e.KeyCode = 40 Then
+        If e.KeyCode = 50 Then
             P2Down = False
-        End If
-    End Sub
-
-    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
-        If e.KeyCode = 65 Then
-            P1Left = True
-        End If
-        If e.KeyCode = 68 Then
-            P1Right = True
-        End If
-        If e.KeyCode = 87 Then
-            P1Up = True
-        End If
-        If e.KeyCode = 83 Then
-            P1Down = True
-        End If
-        If e.KeyCode = 37 Then
-            P2Left = True
-        End If
-        If e.KeyCode = 39 Then
-            P2Right = True
-        End If
-        If e.KeyCode = 38 Then
-            P2Up = True
-        End If
-        If e.KeyCode = 40 Then
-            P2Down = True
-        End If
-
-        If e.KeyCode = 17 Then
-            shooting = True
-            If shooting = True Then
-                draw.DrawLine(bullet, 20000, 20000, 100, 100)
-
-                If e.KeyCode = 32 Then
-                    shooting = True
-                    If shooting = True Then
-                        draw.DrawLine(bullet2, 10, 10, 10, 10)
-
-                    End If
-                End If
-            End If
         End If
     End Sub
 End Class
